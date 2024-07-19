587aba14d6af1aa983ce6c92f977e1841682beb0
Alcuavi <alberto.cuvi@gmail.com>
2024-07-19 12:31:27 +0200

    Added button and controller to load/update countries through the API

---
+++ src/Controller/LoadController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class LoadController extends AbstractController
+{
+    #[Route('/load-countries', name: 'app_country_load', methods: ['GET'])]
+    public function loadCountries(KernelInterface $kernel): Response
+    {
+        $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'app:load-countries',
+        ]);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $content = $output->fetch();
+
+        return new Response("<pre>$content</pre>");
+    }
+}
