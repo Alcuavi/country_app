b9595e55c318a6f092039a94cb8e83889842f177
Alcuavi <alberto.cuvi@gmail.com>
2024-07-19 12:15:23 +0200

    Created CurrencyType.php to define the form for managing currency data and modified CountryType.php to define the form for managing country data

--- src/Form/CountryType.php
+++ src/Form/CountryType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Country;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,21 +14,69 @@ class CountryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('code')
-            ->add('population')
-            ->add('flag')
-            ->add('region')
-            ->add('subregion')
-            ->add('capital')
-            ->add('area')
-            ->add('languages')
-            ->add('borders')
-            ->add('nativeName')
-            ->add('numericCode')
-            ->add('currencies')
-            ->add('timezones')
-        ;
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('code', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('population', null, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('flag', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('region', TextType::class, [
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('subregion', TextType::class, [
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('capital', TextType::class, [
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('area', null, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('languages', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('borders', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('nativeName', TextType::class, [
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('numericCode', TextType::class, [
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('currencies', CollectionType::class, [
+                'entry_type' => CurrencyType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('timezones', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'required' => false,
+                'attr' => ['class' => 'form-control']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
---
+++ src/Form/CurrencyType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CurrencyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('symbol', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ]);
+    }
+}
